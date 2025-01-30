071171e376dfdadf25353368f85eebace3f5df13
Mustakim <mustakimsayyed50@gmail.com>
2025-01-30 01:15:13 +0000

    Changes in Main

--- DSA_Practice/src/Main.java
+++ DSA_Practice/src/Main.java
@@ -3,16 +3,5 @@ import PrefixSum.Range_Sum_Query_Immutable;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*** Range Sum Query Immutable ***/
-
-        /*
-        int narray[] = new int[]{-2, 0, 3, -5, 2, -1};
-        Range_Sum_Query_Immutable numArray = new Range_Sum_Query_Immutable(narray);
-        System.out.println("The sum is:" + numArray.sumRange(0,2));
-        */
-
-
-
     }
 }
